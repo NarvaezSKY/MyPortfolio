7269926d5704cafbe2cdf7528fbfd0b60388715f
Cristian Narvaez <crissnar12xd@gmail.com>
2026-09-23 22:59:34 -0500

    feat: add /robots.txt easter egg route

---
+++ src/app/shared/components/robots-txt/RobotsTxt.tsx
@@ -0,0 +1,9 @@
+export const RobotsTxt = () => {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-[#080c12]">
+      <p className="text-white font-mono text-sm sm:text-base tracking-widest text-center px-4">
+        you shouldn't be here
+      </p>
+    </div>
+  );
+};
--- src/app/shared/router/App.tsx
+++ src/app/shared/router/App.tsx
@@ -1,12 +1,14 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { Route, Routes } from "react-router-dom";
 import { GeneralRouter } from ".";
+import { RobotsTxt } from "../components";
 
 export const  App=()=> {
   return (
     <>
       <NextUIProvider>
         <Routes>
+          <Route path="/robots.txt" element={<RobotsTxt />} />
           <Route path="/*" element={<GeneralRouter/>} />
         </Routes>
       </NextUIProvider>
